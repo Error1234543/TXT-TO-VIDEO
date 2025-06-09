82db8c1045f07a1ec1243bea130439629201784d
Error1234543 <ptaviyad21@gmail.com>
2025-06-09 19:53:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 # DON'T add anything here just add in render's secret or env section 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "20619533"))
+API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7270825299:AAFGI_JSn7AXjejfhPzZkPQHWa_gCDVfoP8")
